72e2e2ca8fe598452c9f3a06751cf6f713b3ec8c
bsicay10 <bsicay@pacifiko.com>
2025-08-02 00:55:42 -0600

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Visual_Castle_Restaurant/' : '/',
+  base: '/VisualCastleRestaurant/',  
 })
