1fbf428ea6bd36efbc9e11a6f0c16eb7b70c3494
bsicay10 <bsicay@pacifiko.com>
2025-08-02 01:10:30 -0600

    udpate action

--- src/Components/Redux/action.js
+++ src/Components/Redux/action.js
@@ -10,10 +10,194 @@ const getDataFailure =()=>{
     return {type:types.GETDATAFAILURE}
 }
 
-
-
-
-
+// Datos hardcodeados como fallback
+const fallbackData = {
+  "briyani": [
+    {
+     "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pepián de Pollo",
+      "qty": "",
+      "price": "75",
+      "count": 0
+    },
+    {
+     "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pepián de Res",
+      "qty": "",
+      "price": "85",
+      "count": 0
+    },
+    {
+     "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Kak'ik (Sopa de Pavo)",
+      "qty": "",
+      "price": "65",
+      "count": 0
+    },
+    {
+     "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Jocón de Pollo",
+      "qty": "",
+      "price": "60",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Hilachas (Carne Deshebrada)",
+      "qty": "",
+      "price": "70",
+      "count": 0
+    }
+  ],
+  "starter": [
+    {
+     "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Rellenitos de Plátano",
+      "qty": "",
+      "price": "25",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Chiles Rellenos",
+      "qty": "",
+      "price": "30",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Paches (Tamal de Papa)",
+      "qty": "",
+      "price": "20",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Tamales Colorados",
+      "qty": "",
+      "price": "15",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Empanadas de Cambray",
+      "qty": "",
+      "price": "18",
+      "count": 0
+    }
+  ],
+  "maincourse": [
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Subanik (Estofado de Pollo)",
+      "qty": "",
+      "price": "80",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Hilachas", 
+      "qty": "",
+      "price": "70",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Jocón de Pollo",
+      "qty": "",
+      "price": "60",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Kak'ik",
+      "qty": "",
+      "price": "65",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pepián de Res",
+      "qty": "",
+      "price": "85",
+      "count": 0
+    }
+  ],
+  "maincoursev": [
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Chiles Rellenos Vegetarianos",
+      "qty": "",
+      "price": "30",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Fiambre Vegetariano",
+      "qty": "",
+      "price": "50",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Revoltijo de Papa",
+      "qty": "",
+      "price": "35",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Atol de Elote",
+      "qty": "",
+      "price": "15",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Tamales de Elote",
+      "qty": "",
+      "price": "20",
+      "count": 0
+    }
+  ],
+  "Complemento": [
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pan Chapín",
+      "qty": "",
+      "price": "10",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Chuchitos",
+      "qty": "",
+      "price": "15",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Tostadas de Maíz",
+      "qty": "",
+      "price": "8",
+      "count": 0
+    },
+    {
+      "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pan de Elote",
+      "qty": "",
+      "price": "12",
+      "count": 0
+    },
+    {
+        "img": "https://www.guatemala.com/fotos/201805/Pepian-guatemalteco-acariciamesp1-885x500.jpg",
+      "name": "Pan de Banano",
+      "qty": "",
+      "price": "10",
+      "count": 0
+    }
+  ]
+};
 
 export const getData = () => dispatch => {
   dispatch(getDataRequest());
@@ -30,24 +214,10 @@ export const getData = () => dispatch => {
 
   const baseUrl = getBaseUrl();
   const dbUrl = `${baseUrl}/db.json`;
-  const testUrl = `${baseUrl}/test.json`;
   
   console.log('Intentando cargar db.json desde:', dbUrl);
   
-  // Primero probar el archivo de test
-  fetch(testUrl)
-    .then(r => {
-      console.log('Test URL response:', r.status, r.statusText);
-      if (r.ok) {
-        return r.json();
-      }
-      throw new Error(`Test failed: ${r.status}`);
-    })
-    .then(testData => {
-      console.log('Test successful:', testData);
-      // Si el test funciona, intentar cargar db.json
-      return fetch(dbUrl);
-    })
+  fetch(dbUrl)
     .then(r => {
       if (!r.ok) {
         console.error('Error al cargar db.json:', r.status, r.statusText);
@@ -61,21 +231,9 @@ export const getData = () => dispatch => {
     })
     .catch(err => {
       console.error('Error al cargar db.json:', err);
-      // Intentar cargar directamente sin base URL como fallback
-      console.log('Intentando cargar db.json sin base URL...');
-      fetch('/db.json')
-        .then(r => {
-          if (!r.ok) throw new Error(`Fallback failed: ${r.status}`);
-          return r.json();
-        })
-        .then(d => {
-          console.log('db.json cargado con fallback:', d);
-          dispatch(getDataSuccess(d));
-        })
-        .catch(fallbackErr => {
-          console.error('Fallback también falló:', fallbackErr);
-          dispatch(getDataFailure());
-        });
+      console.log('Usando datos hardcodeados como fallback...');
+      // Usar datos hardcodeados como fallback
+      dispatch(getDataSuccess(fallbackData));
     });
 };
 
